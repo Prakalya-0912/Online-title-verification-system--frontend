6e9f4234f86f4d324159827ec17e343cb3298dcc
Prakalya Singaravelu <kstharunvenkat6tharun@gmail.com>
2026-08-04 22:31:52 +0530

    Restore routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
-import Login from "./components/Login";
 import Register from "./components/Register";
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import TitleSubmission from "./components/TitleSubmission";
 import Result from "./components/Result";
@@ -11,25 +11,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Home */}
         <Route path="/" element={<Home />} />
 
-        {/* Register */}
         <Route path="/register" element={<Register />} />
 
-        {/* Login */}
         <Route path="/login" element={<Login />} />
 
-        {/* Dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
 
-        {/* Submit Project Title */}
         <Route path="/submit" element={<TitleSubmission />} />
 
-        {/* Result */}
         <Route path="/result" element={<Result />} />
-
       </Routes>
     </BrowserRouter>
   );
